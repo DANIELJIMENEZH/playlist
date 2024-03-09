7bfeb444daa05a626f80d5646248e612177be886
DANIELJIMENEZH <159660355+DANIELJIMENEZH@users.noreply.github.com>
2024-03-09 12:19:08 -0600

    Add files via upload

---
+++ playlist.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.playlist;
+/**
+ *
+ * @author FER
+ */
+public class Playlist {
+
+    public static void main(String[] args){
+        Musicas p1 = new Musicas ("El paso del gigante", "Cumbias Mexicanas", "1983",3);
+        p1.ShowPlaylist();
+    }
+}
